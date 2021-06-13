06697a56f56a9b26f941f80d90e0881b9fdd31da
Mahadi001 <sadat.himel@gmail.com>
2021-06-13 13:46:43 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function App() {
+    const sayHello = () => {
+        console.log("hello");
+    };
+
     return (
         <div>
             <h1>Hello React from coders time</h1>
+            <button onClick={sayHello}>Hello</button>
         </div>
     )
 }
