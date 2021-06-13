bbed5c5149b23941d0a351d12c17feba527f5195
Mahadi001 <sadat.himel@gmail.com>
2021-06-13 18:12:37 +0600

    toggle app and add some useState and props

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ function App() {
 
     const increment = () => {
         setCount(count + 1);
+        setRed(!isRed);
     }
 
     return (
         <div className='app'>
+            <h1 className = {isRed ? "red" : "" }>Change my color!</h1>
             <button onClick={increment}>Increment</button>
             <h1>{count}</h1>
         </div>
