c1933c8b8f787d4dbef0abaed2325e63a3a9de4d
Mahadi001 <sadat.himel@gmail.com>
2021-06-13 13:57:41 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Hello from './sayHello';
 
 function App() {
-    const sayHello = () => {
-        console.log("hello");
-    };
-
+    
     return (
         <div>
-            <h1>Hello React from coders time</h1>
-            <button onClick={sayHello}>Hello</button>
+            <h1>This is from app component</h1>
+            <Hello />
         </div>
     )
 }
