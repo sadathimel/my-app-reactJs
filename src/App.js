b6efad0157b1667fb39b2e00227cfa2fba4b8bb5
Mahadi001 <sadat.himel@gmail.com>
2021-06-13 16:40:40 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Hello from './sayHello';
+import Tweet from './Tweet';
 
 function App() {
     
     return (
-        <div>
-            
+        <div className='app'>
+            <Tweet name="Sadat Himel" message="Hi i'm Sadat Himel"/>
+            <Tweet name="Tanvir Ahmed" message="Hi i'm Tanvir ahmed"/>
+            <Tweet name="Saidul Hasan" message="Hi i'm Saidul Hassen"/>
+            <Tweet name="Alomgir Hossen" message="Hi i'm Ahlomgir"/>
         </div>
     )
 }
